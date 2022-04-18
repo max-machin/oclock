a5d8020fb5dbd944d950f89ad5604b9a1a72ab44
max-machin <machin.max@lapalteforme.io>
2022-04-18 20:36:40 +0200

    Création de la page du minuteur ainsi que de la navabar et de l'index

---
+++ minuteur.php
@@ -0,0 +1,4 @@
+<?php
+require 'ressources/header.php';
+?>
+<h1>Minuteur</h1>
--- ressources/header.php
+++ ressources/header.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;600;800&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="stylesheet" href="style/style.css">
-    <title>Document</title>
+    <title>O'Clock</title>
 </head>
-<body>
+<body class="light">
     <nav>
+        <div  class="home">
+            <a href="index.php"><i class="fa-solid fa-house-chimney"></i></a>
+        </div>
         <ul>
-            <li><a href="">Minuteur</a></li>
+            <li><a href="minuteur.php">Minuteur</a></li>
             <li><a href="">Chronomètre</a></li>
             <li><a href="">Réveil</a></li>
             <li><a href="">Horloge</a></li>
