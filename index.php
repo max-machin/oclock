43b797283f8418efe09ff9dc75e828674e1b8977
max-machin <machin.max@lapalteforme.io>
2022-04-25 02:28:53 +0200

    Ajout de l'index

--- index.php
+++ index.php
@@ -1,8 +1,37 @@
 <?php
 require 'ressources/header.php';
 ?>
-
-
+<main>
+    <h1 class="nom">O'clock</h1>
+    <section class="index">
+        <article class="article_index">
+            <div>
+                <h2>C'est votre </h2>
+                <span class="output"></span>
+            </div>
+            
+            <article class="grid">
+                <div>
+                    <i class="fa-solid fa-bell"></i>
+                    <p>Réveil</p>
+                </div>
+                <div>
+                    <i class="fa-solid fa-stopwatch"></i>
+                    <p>Minuteur</p>
+                </div>
+                <div>
+                    <i class="fa-solid fa-flag"></i>
+                    <p>Chronomètre</p>
+                </div>
+                <div>
+                    <i class="fa-solid fa-clock"></i>
+                    <p>Horloge</p>
+                </div>
+            </article>
+            <p>Controllez le temps avec une main</p>
+        </article>
+    </section>
+</main>
 <?php
 require 'ressources/footer.php';
 ?>
--- js/style.js
+++ js/style.js
@@ -44,4 +44,70 @@ window.addEventListener("DOMContentLoaded", (event) => {
         dark.classList.add('none')
 
     }
+
+    var output = document.querySelector('.output')
+
+    var res
+
+    var typeSpeed = 300
+
+    var removeSpeed = 150
+
+    var id = 0
+
+    var mots = [
+        "heure ...",
+        "moment !",
+        "réveil",
+        "tour",
+        "meilleur temps"
+    ]
+
+    var colors = [
+        "#8208e6",
+        "#ff0256",
+        "#00b84c",
+        "#8208e6",
+        "#ff0256"
+    ]
+
+    var count = 0
+
+    let time = setInterval(type, typeSpeed)
+
+    function type(){
+        res = mots[id].substr(0, count)
+        output.style.color = colors[id]
+
+        if (count >= mots[id].length + 6){
+            clearInterval(time)
+
+            count = 1
+
+            time = setInterval(remove, removeSpeed)
+        }
+        output.innerHTML = res
+
+        count++
+    }
+
+    function remove(){
+        res = mots[id].substr(0, mots[id].length - count)
+
+        if (res.length <= 0){
+            if (id >= mots.length - 1){
+                id = 0
+            } else {
+                id++
+            }
+            clearInterval(time)
+
+            count = 0
+
+            time = setInterval(type, typeSpeed)
+        }
+
+        output.innerHTML = res
+        count++
+    }
 })
